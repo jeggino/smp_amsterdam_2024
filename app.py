a19b1f855172b0d4da3f4d2cb21b237c2f718c8e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-07-09 10:16:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -150,31 +150,31 @@ chart_number_1 = st.altair_chart(total, use_container_width=True, theme=None, ke
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
 if "data" not in st.session_state:
     st.session_state.data = pd.DataFrame(
         np.random.randn(20, 3), columns=["a", "b", "c"]
     )
+df = st.session_state.data
 
-spec = {
-    "mark": {"type": "circle", "tooltip": True},
-    "params": [
-        {"name": "interval_selection", "select": "interval"},
-        {"name": "point_selection", "select": "point"},
-    ],
-    "encoding": {
-        "x": {"field": "a", "type": "quantitative"},
-        "y": {"field": "b", "type": "quantitative"},
-        "size": {"field": "c", "type": "quantitative"},
-        "color": {"field": "c", "type": "quantitative"},
-        "fillOpacity": {
-            "condition": {"param": "point_selection", "value": 1},
-            "value": 0.3,
-        },
-    },
-}
+point_selector = alt.selection_point("point_selection")
+interval_selector = alt.selection_interval("interval_selection")
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(
+        x="a",
+        y="b",
+        size="c",
+        color="c",
+        tooltip=["a", "b", "c"],
+        fillOpacity=alt.condition(point_selector, alt.value(1), alt.value(0.3)),
+    )
+    .add_params(point_selector, interval_selector)
+)
 
-event = st.vega_lite_chart(st.session_state.data, spec, key="vega_chart", on_select="rerun")
+event = st.altair_chart(chart, key="alt_chart", on_select="rerun")
 
 event
 
